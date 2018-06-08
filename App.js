b780e16c297b9b9e3e178382ca862e1fadc14f9b
Jane <986736788@qq.com>
2018-06-08 11:14:07 +0800

    学习
    新学的一些东西哈

--- App.js
+++ App.js
@@ -9,30 +9,33 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  TextInput
 } from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
+  'Cmd+D or shake for dev menu',
   android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+  'Shake or press menu button for dev menu',
 });
 
 type Props = {};
 export default class App extends Component<Props> {
+  constructor(props) {
+    super(props)
+    this.state = { text: '' }
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-          希望我能继续学习RN 混合开发哦rr
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
+        <Text style={styles.instructions}>Welcome to learn React Native</Text>
+        <Text style={styles.welcome}>Hello {this.props.name}</Text>
+        <TextInput style={styles.inputTexts} placeholder="please input "
+                   onChangeText={(text) => this.setState({ text })}></TextInput>
+        <Text>
+          {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
         </Text>
       </View>
     );
@@ -42,18 +45,30 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    // height: 630,
+    justifyContent: 'space-between',
+    alignItems: 'stretch',
     backgroundColor: '#F5FCFF',
+    flexDirection: 'column'
+
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
+    color: '#00ff00',
+
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
   },
+  inputTexts: {
+    textAlign: 'center',
+    flex: 4,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  }
 });
--- README.md
+++ README.md
@@ -10,4 +10,27 @@ RN中文网
 
 https://reactnative.cn/docs/0.51/getting-started.html
 
+1.prop属性：大多数组件在创建时就可以使用各种参数来进行定制。用于定制的这些参数就称为props（属性）。
 
+自定义的组件也可以使用props。通过在不同的场景使用不同的属性定制，可以尽量提高自定义组件的复用范畴。只需在render函数中引用this.props，然后按需处理即可。
+
+2.Flex的使用 注意项：
+
+组件能够撑满剩余空间的前提是其父容器的尺寸不为零。如果父容器既没有固定的width和height，也没有设定flex，则父容器的尺寸为零。其子组件如果使用了flex，也是无法显示的。
+3.Flexbox
+
+React Native中使用flexbox规则来指定某个组件的子元素的布局。
+
+Flexbox可以在不同屏幕尺寸上提供一致的布局结构。
+
+一般来说，使用flexDirection、alignItems和 justifyContent三个样式属性就已经能满足大多数布局需求。
+
+flexDirection:可以决定布局的主轴。子元素是应该沿着水平轴(row)方向排列，还是沿着竖直轴(column)方向排列呢？默认值是竖直轴(column)方向。
+
+justifyContent:决定其子元素沿着主轴的排列方式,flex-start、center、flex-end、space-around以及space-between
+
+alignItems:决定其子元素沿着次轴（与主轴垂直的轴，比如若主轴方向为row，则次轴方向为column）的排列方式。对应的这些可选项有：flex-start、center、flex-end以及stretch
+
+注意：要使stretch选项生效的话，子元素在次轴方向上不能有固定的尺寸。以下面的代码为例：只有将子元素样式中的width: 50去掉之后，alignItems: 'stretch'才能生效。
+
+解释一下stretch:弹性元素被在侧轴方向被拉伸到与容器相同的高度或宽度。
